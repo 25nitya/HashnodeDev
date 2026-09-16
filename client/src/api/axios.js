88f3100146ac2641677f22719df0b2eb98f5fce9
25nitya <nityagupta453@gmail.com>
2026-09-16 14:44:04 +0530

    fix: update frontend API URL to live render backend

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api',
+  baseURL: 'https://hashnodedev.onrender.com',
 });
 
 // Attach JWT token to every outgoing request
